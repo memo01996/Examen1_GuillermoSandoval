9077282436a6979ddf82c547b31cce0c25976822
memo01996 <guillermo_asf@live.com>
2018-02-23 18:16:43 -0600

    opciones

--- src/examen1_guillermosandoval/Examen1_GuillermoSandoval.java
+++ src/examen1_guillermosandoval/Examen1_GuillermoSandoval.java
@@ -49,14 +49,14 @@ public class Examen1_GuillermoSandoval {
                 throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
             }
         });
-        System.out.println(usuario + "/" + carpetaraiz);
+        System.out.println(usuario + "/" + carpetaraiz + "/");
         do {
             ingresa = sc.next();
             if (ingresa.contains("mkdir")) {
                 carpetaraiz = ingresa.substring(5, ingresa.length() - 2);
                 for (Object temp : lista) {
                     for (int i = 0; i < lista.size(); i++) {
-                        if (((Carpeta) lista.get(i)).getLista1().contains(ingresa)) {
+                        if (((Carpeta) lista.get(i)).getLista1().contains(carpetaraiz)) {
                             ((Carpeta) lista.get(i)).getLista1().add(new Archivo(carpetaraiz) {
                                 @Override
                                 public void fecha_creacion() {
@@ -73,7 +73,12 @@ public class Examen1_GuillermoSandoval {
                     }
                 }
             } else if (ingresa.contains("cat") && ingresa.contains(".txt")) {
-                carpetaraiz = ingresa.substring(4, ingresa.length() - 3);
+                carpetaraiz = ingresa.substring(3, ingresa.length() - 2);
+                System.out.println("Ingrese texto");
+                String texto = sc.next();
+                for (int i = 0; i < lista.size(); i++) {
+                    ((Carpeta) lista.get(i)).getLista1().add(new ArchivodeTexto(texto) + ".txt");
+                }
             } else if (ingresa.contains("cat") && ingresa.contains(".exec")) {
 
             } else if (ingresa.contains("mod")) {
@@ -81,11 +86,26 @@ public class Examen1_GuillermoSandoval {
             } else if (ingresa.contains("cd")) {
 
             } else if (ingresa.equalsIgnoreCase("cd..")) {
-
+                for (Object object : lista) {
+                    for (int i = 0; i < lista.size(); i++) {
+                        System.out.println(((Archivo)lista.get(i)).getNombre());
+                    }
+                }
             } else if (ingresa.equalsIgnoreCase("ls")) {
-
+                for (Object temp : lista) {
+                    for (int i = 0; i < lista.size(); i++) {
+                        if (((Carpeta) lista.get(i)).getLista1().equals(temp)) {
+                            System.out.println(((Carpeta) lista.get(i)).getLista1().get(i));
+                        }
+                    }
+                }
             } else if (ingresa.contains("del")) {
-
+                carpetaraiz = ingresa.substring(3, ingresa.length() - 2);
+                for (int i = 0; i < lista.size(); i++) {
+                    if (((Archivo) lista.get(i)).getNombre().equalsIgnoreCase(carpetaraiz)) {
+                        lista.remove(i);
+                    }
+                }
             } else if (ingresa.equalsIgnoreCase("exec")) {
 
             }
